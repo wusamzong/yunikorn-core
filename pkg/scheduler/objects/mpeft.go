d052d93bdf02fd279c63e75ebc2d6b0bed59f27d
wusamzong <t3742238@gmail.com>
2024-07-16 02:45:39 +0800

    update

--- pkg/scheduler/objects/mpeft.go
+++ pkg/scheduler/objects/mpeft.go
@@ -636,7 +636,7 @@ func (m *mpeft) decideNode(j *Job) bool {
 			j.replicaCpu = selectNode.cpu
 			j.replicaMem = selectNode.mem
 			for _, a := range r.actions{
-				a.executionTime = a.executionTime * float64(originalCPU+originalMem)/float64(selectNode.cpu+selectNode.mem) //* 0.75
+				a.executionTime = a.executionTime * float64(originalCPU+originalMem)/float64(selectNode.cpu+selectNode.mem)
 			}
 		}
 		doneReplica = append(doneReplica, r)
